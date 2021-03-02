9c02ed9025aca83f216f6f4341440a9c2299e152
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-02 00:25:07 -0800

    fetching movie data from omdbapi

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hi there!');
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'be370d58',
+            s: 'avengers'
+        }
+    });
+
+    console.log(response.data);
+}
+
+fetchData();
