03dacb98690130c95d0e6efa84e216f52937f472
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-02 16:14:57 -0800

    adding a logic to handle error if no record found

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const fetchData = async (searchTerm) => {
             s: searchTerm
         }
     });
-
+    
+    if(response.data.Error){
+        return [];
+    }
     return response.data.Search;
 }
 
@@ -13,7 +16,18 @@ const input = document.querySelector('input')
 
 const onInput = async event => {
     const movies = await fetchData(event.target.value)
-    console.log(movies);
+
+    for(let movie of movies){
+        const div = document.createElement('div')
+        
+        div.innerHTML = `
+            <img src="${movie.Poster}" />
+            <h1>${movie.Title}</h1>
+        `;
+        
+
+        document.querySelector('#target').appendChild(div);
+    }
 };
 
 input.addEventListener('input', debounce(onInput, 500));
