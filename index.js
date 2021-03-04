32945fc3031407fe42b9c2f7eb7b02f9baab22b6
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-04 13:12:04 -0800

    assigning attributes from strings to numbers and floats to make comparison calclulations

--- index.js
+++ index.js
@@ -72,9 +72,17 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 
 const runComparison = () => {
     console.log('Time for comparison')
+
 }
 
 const movieTemplate = (movieDetail) => {
+
+    const dollars = parseInt(movieDetail.BoxOffice.replace(/\$/g, '').replace(/,/g, ''));
+    const metascore = parseInt(movieDetail.Metascore);
+    const imdbRating = parseFloat(movieDetail.imdbRating);
+    const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g,''));
+    console.log(dollars, metascore, imdbRating, imdbVotes);
+
     return `
         <article class="media">
             <figure class="media-left">
