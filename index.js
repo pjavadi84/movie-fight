4ee1797e8b345bd74d004569069159ea1219a78b
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-04 13:50:13 -0800

    fixed bugs for datasets that compare two strings instead of values

--- index.js
+++ index.js
@@ -71,25 +71,28 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 }
 
 const runComparison = () => {
-    const leftSideStats = document.querySelectorAll("#left-summary .notification");
-    const rightSideStats = document.querySelectorAll("#right-summary .notification");
-
-    leftSideStats.forEach((leftStat, index)=>{
-        const rightStat = rightSideStats[index];
-
-        const leftSideValue = leftStat.dataset.value;
-        const rightSideValue = rightStat.dataset.value;
-
-        if(rightSideValue > leftSideValue){
-            leftStat.classList.remove('is-primary');
-            leftStat.classList.add('is-warning');
-        } else {
-            rightStat.classList.remove('is-primary');
-            rightStat.classList.add('is-warning');
-        }
-    })
-
-}
+    const leftSideStats = document.querySelectorAll(
+      '#left-summary .notification'
+    );
+    const rightSideStats = document.querySelectorAll(
+      '#right-summary .notification'
+    );
+   
+    leftSideStats.forEach((leftStat, index) => {
+      const rightStat = rightSideStats[index];
+   
+      const leftSideValue = leftStat.dataset.value; // <------ BAD
+      const rightSideValue = rightStat.dataset.value; // <------ BAD
+   
+      if (rightSideValue > leftSideValue) {
+        leftStat.classList.remove('is-primary');
+        leftStat.classList.add('is-warning');
+      } else {
+        rightStat.classList.remove('is-primary');
+        rightStat.classList.add('is-warning');
+      }
+    });
+  };
 
 const movieTemplate = (movieDetail) => {
 
