9a20d284fa781aa5b92ea09af175f18e7ab454ef
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-03-04 13:27:05 -0800

    parsing all attribute values into data-value

--- index.js
+++ index.js
@@ -81,7 +81,14 @@ const movieTemplate = (movieDetail) => {
     const metascore = parseInt(movieDetail.Metascore);
     const imdbRating = parseFloat(movieDetail.imdbRating);
     const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g,''));
-    console.log(dollars, metascore, imdbRating, imdbVotes);
+    const awards = movieDetail.Awards.split(' ').reduce((prev, word)=>{
+        const value = parseInt(word);
+        if(isNaN(value)){
+            return prev;
+        } else {
+            return prev + value;
+        }
+    }, 0);
 
     return `
         <article class="media">
@@ -99,27 +106,27 @@ const movieTemplate = (movieDetail) => {
             </div>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value= ${awards} class="notification is-primary">
             <p class="title">${movieDetail.Awards}</p>
             <p class="subtitle">Awards</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${dollars} class="notification is-primary">
             <p class="title">${movieDetail.BoxOffice}</p>
             <p class="subtitle">Box Office</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${metascore} class="notification is-primary">
             <p class="title">${movieDetail.Metascore}</p>
             <p class="subtitle">Metascore</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${imdbRating} class="notification is-primary">
             <p class="title">${movieDetail.imdbRating}</p>
             <p class="subtitle">IMDB Rating</p>
         </article>
 
-        <article class="notification is-primary">
+        <article data-value=${imdbVotes} class="notification is-primary">
             <p class="title">${movieDetail.imdbVotes}</p>
             <p class="subtitle">IMDB Votes</p>
         </article>
